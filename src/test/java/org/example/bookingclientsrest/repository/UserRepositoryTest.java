5acb42d70bd90c02b3e49f5e9aacc604e44db1bb
mipowka <mipowkaa@gmail.com>
2024-10-18 18:07:43 +0300

    Finished all tests for Repository

--- src/test/java/org/example/bookingclientsrest/repository/UserRepositoryTest.java
+++ src/test/java/org/example/bookingclientsrest/repository/UserRepositoryTest.java
@@ -6,14 +6,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.stereotype.Repository;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@DataJpaTest
-@ActiveProfiles("test")
+import java.util.Optional;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
@@ -21,6 +20,7 @@ class UserRepositoryTest {
 
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
@@ -29,7 +29,7 @@ class UserRepositoryTest {
     }
 
     @Test
-    void save(){
+    void save() {
         User user = new User();
         user.setUsername("admin");
         user.setPassword("admin");
@@ -39,5 +39,52 @@ class UserRepositoryTest {
         Assertions.assertThat(save).isNotNull();
         Assertions.assertThat(save.getUsername()).isEqualTo(user.getUsername());
         Assertions.assertThat(save.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void findById() {
+
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+
+        User savedUser = userRepository.save(user);
+        User userFromDb = userRepository.findById(savedUser.getId()).get();
+
+        Assertions.assertThat(userFromDb).isNotNull();
+        Assertions.assertThat(userFromDb.getUsername()).isEqualTo(user.getUsername());
+        Assertions.assertThat(userFromDb.getId()).isEqualTo(savedUser.getId());
+
+    }
+
+    @Test
+    void delete() {
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+
+        User savedUser = userRepository.save(user);
+        userRepository.delete(savedUser);
+        Optional<User> byId = userRepository.findById(savedUser.getId());
+
+        Assertions.assertThat(byId).isNotPresent();
+    }
+
+    @Test
+    void update() {
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+
+        User savedUser = userRepository.save(user);
+        Optional<User> byId = userRepository.findById(savedUser.getId());
+
+        byId.get().setUsername("NeAdmin");
+
+        User updatedUser = userRepository.save(byId.get());
+
+        Assertions.assertThat(updatedUser).isNotNull();
+        Assertions.assertThat(updatedUser.getUsername()).isEqualTo("NeAdmin");
+        Assertions.assertThat(updatedUser.getPassword()).isEqualTo(user.getPassword());
     }
 }
